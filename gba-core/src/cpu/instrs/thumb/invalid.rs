6445af7bdee5e07b0aea42cf6dce5eff71ac9792
BusinessJoe <lukasboelling002@gmail.com>
2024-04-15 19:16:34 -0400

    Show PC on invalid thumb instruction

--- gba-core/src/cpu/instrs/thumb/invalid.rs
+++ gba-core/src/cpu/instrs/thumb/invalid.rs
@@ -3,8 +3,9 @@ use super::ThumbInstruction;
 pub struct Invalid;
 
 impl ThumbInstruction for Invalid {
-    fn execute(&self, _: &mut crate::cpu::Cpu, _: &mut crate::bus::Bus, instruction: u16) {
-        panic!("Executed invalid thumb instruction {:018b}", instruction);
+    fn execute(&self, cpu: &mut crate::cpu::Cpu, _: &mut crate::bus::Bus, instruction: u16) {
+        // PC might be off
+        panic!("Executed invalid thumb instruction {:016b} at PC {:X}", instruction, cpu.get_reg(15));
     }
 
     fn disassembly(&self, _: u16) -> String {
